41be33beca1d64756dc4e7380048390fef1e0c7f
duyanming <dym880@163.com>
2020-05-07 09:49:45 +0800

    多线程测试

--- annosimulateclient/src/main/java/AnnoSimulateClientBoot.java
+++ annosimulateclient/src/main/java/AnnoSimulateClientBoot.java
@@ -1,16 +1,30 @@
 import anno.thrift.client.DefaultConfigManager;
 import simulate.requestTest;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CyclicBarrier;
+
 public class AnnoSimulateClientBoot {
     public static void main(String[] args) throws Exception {
         DefaultConfigManager.SetDefaultConnectionPool(200, 4, 50);
         DefaultConfigManager.SetDefaultConfiguration("AnnoClient", "127.0.0.1", 6660);
-
-        for (int i = 0; i < 20; i++) {
-            new requestTest().Request1();
+        CyclicBarrier cb = new CyclicBarrier(2000+1 );
+        for (int i = 0; i < 2000; i++) {
+            int finalI = i;
+            new Thread(() -> {
+                try {
+                    System.out.println("task begin:" + finalI);
+                    new requestTest().Request1();
+                    System.out.println("task end:" + finalI);
+                    cb.await();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }).start();
 //            new requestTest().Request1AnnoNet(i);
-            System.out.println("End"+i);
         }
+       cb.await();
         System.out.println("End All");
     }
 }
