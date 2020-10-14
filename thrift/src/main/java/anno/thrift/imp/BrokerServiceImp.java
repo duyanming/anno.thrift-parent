19ad970fa5637e835faed6c34d592819092b039f
duyanming <dym880@163.com>
2020-10-14 17:36:59 +0800

    独立监控方法

--- thrift/src/main/java/anno/thrift/imp/BrokerServiceImp.java
+++ thrift/src/main/java/anno/thrift/imp/BrokerServiceImp.java
@@ -16,10 +16,13 @@ public class BrokerServiceImp implements BrokerService.Iface {
   @Override
   public String broker(Map<String, String> input) {
     try {
-        if(input.getOrDefault(Eng.NAMESPACE,"").equals("Anno.Plugs.Trace")
+        if((input.getOrDefault(Eng.NAMESPACE,"").equals("Anno.Plugs.Trace")
                 &&input.getOrDefault(Eng.CLASS,"").equals("Trace")
                 &&input.getOrDefault(Eng.METHOD,"").equals("GetServerStatus")
-        ){
+        )||(input.getOrDefault(Eng.NAMESPACE,"").equals("Anno.Plugs.Monitor")
+                &&input.getOrDefault(Eng.CLASS,"").equals("Resource")
+                &&input.getOrDefault(Eng.METHOD,"").equals("GetServerStatus")
+        )){
 
             return JSON.toJSONStringWithDateFormat(new ActionResult<ServerStatus>(true,UseSysInfoWatch.GetServerStatus()), "yyyy-MM-dd HH:mm:ss.SSS");
         }
